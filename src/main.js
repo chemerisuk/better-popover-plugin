10511ba8fa45d754024cb774ecb7053b6dec5288
Maksim Chemerisuk <chemerisuk@gmail.com>
2015-02-06 13:57:37 +0300

    simplify code, add more comments and improve demo

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 (function(DOM) {
     DOM.extend("*", {
-        popover: function(content, hpos, vpos) {
-            var popover = this.get("_popover");
+        popover(content, hpos, vpos) {
+            var popover = this.get("_popoverElement");
 
             if (!popover) {
-                popover = DOM.create("div.better-popover-plugin").css("visibility", "hidden");
+                popover = DOM.create("div.btr-popover").css("visibility", "hidden");
 
                 this.before(popover);
 
                 popover.css({
                     // MUST set position:absolute for correct offset calculation
                     "position": "absolute",
-                    "z-index": 1 + (this.css("z-index") | 0)
+                    // increases z-index of the related element by one to be on top
+                    "z-index": this.css("z-index") | 0 + 1
                 });
 
-                this.set("_popover", popover);
+                this.set("_popoverElement", popover);
             }
 
             if (content != null) {
@@ -22,14 +23,11 @@
             }
 
             if (typeof hpos === "string") {
-                // position is "center" by default
-                hpos = hpos || "center";
-                vpos = vpos || "center";
-
                 var offset = this.offset();
                 var popoverOffset = popover.css("margin", "0").offset();
 
                 popover.css({
+                    // set appropriate margins
                     "margin-left": calcLeftMargin(hpos, offset, popoverOffset),
                     "margin-top": calcTopMargin(vpos, offset, popoverOffset)
                 });
@@ -44,11 +42,11 @@
         case "left":
             return offset.left - popoverOffset.left;
 
-        case "center":
-            return offset.left - popoverOffset.left + (offset.width - popoverOffset.width) / 2;
-
         case "right":
             return offset.right - popoverOffset.left - popoverOffset.width;
+
+        default: // position is "center" by default
+            return offset.left - popoverOffset.left + (offset.width - popoverOffset.width) / 2;
         }
     }
 
@@ -57,11 +55,11 @@
         case "top":
             return offset.top - popoverOffset.bottom;
 
-        case "center":
-            return offset.top - popoverOffset.top + (offset.height - popoverOffset.height) / 2;
-
         case "bottom":
             return offset.bottom - popoverOffset.top;
+
+        default: // position is "center" by default
+            return offset.top - popoverOffset.top + (offset.height - popoverOffset.height) / 2;
         }
     }
 }(window.DOM));
